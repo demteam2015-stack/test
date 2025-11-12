11b6a84bf12b760d5b0dac63e5d7de0a12926099
Алексей Демьяненко <av.demyanenko.av@yandex.ru>
2025-11-12 14:12:01 +0000

    изменим дизайн полностью

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Mitr', 'sans-serif'],
-        headline: ['Kanit', 'sans-serif'],
+        body: ['Inter', 'sans-serif'],
+        headline: ['Inter', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
