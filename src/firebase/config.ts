4ee613cfdfb81d3f5700a07678339ab3a993bb1f
Алексей Демьяненко <av.demyanenko.av@yandex.ru>
2025-11-12 12:46:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4776671538-bb345",
+  "appId": "1:655964693857:web:627eb31be51453d147d120",
+  "apiKey": "AIzaSyB4D7ElN4CjT_FptU-APDt4X3u1fNPabag",
+  "authDomain": "studio-4776671538-bb345.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "655964693857"
+};
